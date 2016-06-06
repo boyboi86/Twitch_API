5c1ec4339d18c9d76978d0dd6a76ca3e25bab34d
boyboi86 <boyboi86@gmail.com>
2016-06-06 15:07:31 +0800

    final

--- script.js
+++ script.js
@@ -48,9 +48,9 @@ $(document).ready(function() {
         /*Determine player's Acc status*/
         /*try not to do the below code, I'm just lazy, I'll remake another version using angularjs hint: you need 2 calls to get logo/details/name even for offline mode*/
         if (data.status == 422) {
-          $('#all').append("<div class='player closed'>" + "<div class='img'>" + "<img src='https://cdn1.iconfinder.com/data/icons/simple-icons/2048/twitch-2048-black.png'>" + "</div>" + "<div class='name'>" + data.message.slice(9, 17) + "</div>" + "<div class='status'>" + 'Account Closed' + "</div>" + "</div>")
+          $('#all').append("<div class='player closed'>" + "<div class='img'>" + "<img src='assets/image/black-icon.png'>" + "</div>" + "<div class='name'>" + data.message.slice(9, 17) + "</div>" + "<div class='status'>" + 'Account Closed' + "</div>" + "</div>")
         } else if (data.stream == undefined || data.stream == null) {
-          $('#all, #offline').append("<div class='player offline'>" + "<div class='img'>" + "<img src='https://d1qb2nb5cznatu.cloudfront.net/startups/i/114142-19c0993bf69c468f1350fd422bfad6b2-medium_jpg.jpg?buster=1410211530'>" + "</div>" + "<div class='name'>" + data._links.channel.substring(38) + "</div>" + "<div class='status' style='color:red;'>" + 'offline' + "</div>" + "</div>")
+          $('#all, #offline').append("<div class='player offline'>" + "<div class='img'>" + "<img src='assets/image/purple-icon.jpg'>" + "</div>" + "<div class='name'>" + data._links.channel.substring(38) + "</div>" + "<div class='status' style='color:red;'>" + 'offline' + "</div>" + "</div>")
         } else {
           $('#all, #online').append('<a href="' + data.stream.channel.url + '" target="_blank">' + "<div class='player online'>" + "<div class='img'>" + "<img src='" + data.stream.channel.logo + "'>" + "</div>" + "<div class='name'>" + data.stream.channel.name + "</div>" + "<div class='status' style='color:green;'>" + 'online' + "</div>" + "<div class=game>" + data.stream.channel.game + "</div>" + "</div>" + "</a>")
         };
